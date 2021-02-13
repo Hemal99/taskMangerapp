1647f597187ca69c6fa1405a8bfdc08258d5f822
Hemal <h.githmin@gmail.com>
2021-02-13 16:58:43 +0530

    completed

--- src/components/TasksList/TaskItem.jsx
+++ src/components/TasksList/TaskItem.jsx
@@ -16,8 +16,8 @@ function TaskItem(props) {
         <div className="card-body d-flex flex-column">
           <p className="card-text">{props.taskInfo.description}</p>
           <div className="mt-auto text-right">
-            <button className="btn btn-danger btn-sm mr-2">Delete</button>
-            <button className="btn btn-success btn-sm ">Complete</button>
+            <button className="btn btn-danger btn-sm mr-2" onClick={()=>props.onDelete(props.taskInfo.id)/*parameter ekak thiynwanm function */}>Delete</button>
+            <button className="btn btn-success btn-sm " onClick={()=>props.onComplete(props.taskInfo.id)}>Complete</button>
           </div>
         </div>
       </div>
--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,60 +1,47 @@
-import React, { useState } from 'react';
-import moment from 'moment';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import TaskItem from './TaskItem';
+import TaskItem from "./TaskItem";
 
 function TodoList() {
-  const [tasks, setTasks] = useState([
-    {
-      id: '1',
-      title: 'Task 1',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pulvinar ornare nisi, in hendrerit tellus ultricies et. ',
-      createdDate: new Date(),
-      dueDate: moment().add(3, 'days').format('DD-MM-yyyy'),
-      status: 'New',
-    },
-    {
-      id: '2',
-      title: 'Task 2',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pulvinar ornare nisi, in hendrerit tellus ultricies et. ',
-      createdDate: new Date(),
-      dueDate: moment().add(3, 'days').format('DD-MM-yyyy'),
-      status: 'New',
-    },
-    {
-      id: '3',
-      title: 'Task 3',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pulvinar ornare nisi, in hendrerit tellus ultricies et. ',
-      createdDate: new Date(),
-      dueDate: moment().add(3, 'days').format('DD-MM-yyyy'),
-      status: 'Completed',
-    },
-    {
-      id: '4',
-      title: 'Task 4',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pulvinar ornare nisi, in hendrerit tellus ultricies et. ',
-      createdDate: new Date(),
-      dueDate: moment().add(3, 'days').format('DD-MM-yyyy'),
-      status: 'Completed',
-    },
-    {
-      id: '5',
-      title: 'Task 5',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pulvinar ornare nisi, in hendrerit tellus ultricies et. ',
-      createdDate: new Date(),
-      dueDate: moment().add(3, 'days').format('DD-MM-yyyy'),
-      status: 'New',
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+  const [taskUpdate,setTaskUpdate]=useState(false)
+
+
+  useEffect(() => {
+    const apiUrl =
+      "https://reacttaskmanager-829b9-default-rtdb.firebaseio.com/tasks.json";
+
+    axios
+    .get(apiUrl).then((response) => {
+      if (response.data) {
+        setTasks(Object.values(response.data));
+      }
+    });
+  },[taskUpdate]);//taskUpdate kiyna variable eka wenas sunoth me useEffect eka trigger wenwa
+
+
+  const handleComplete=(taskId)=>{
+    const apiUrl =
+    `https://reacttaskmanager-829b9-default-rtdb.firebaseio.com/tasks/${taskId}.json`;
+
+    axios.patch(apiUrl,{status:"Completed"}).then(res=>{
+      setTaskUpdate(!taskUpdate)
+    })
+  }
+
+  const handleDelete = (taskId) => {
+    const apiUrl =
+    `https://reacttaskmanager-829b9-default-rtdb.firebaseio.com/tasks/${taskId}.json`;
+
+    axios.delete(apiUrl).then(res=>{
+      setTaskUpdate(!taskUpdate)
+    })
+  }
 
   const displayTasks = () => {
     return tasks.map((task) => {
-      return <TaskItem key={task.id} taskInfo={task} />;
+      return <TaskItem key={task.id} taskInfo={task} onComplete={handleComplete} onDelete={handleDelete} />;
     });
   };
 
